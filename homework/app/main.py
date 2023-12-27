09a23972a70342ca4cd13d2e1b3633b377b65ccf
Mariia Zaripova <zaripova.mary.m@gmail.com>
2023-12-27 18:41:35 +0400

    README.md was added

--- homework/app/main.py
+++ homework/app/main.py
@@ -1,14 +1,20 @@
 from app import app
 from views import AdvertisementView, LoginView, UserView
 
+user_view = UserView.as_view("user")
 advertisements_view = AdvertisementView.as_view("advertisements")
 
 
 app.add_url_rule("/login", view_func=LoginView.as_view("login"), methods=["POST"])
 app.add_url_rule(
+    "/user/registration",
+    view_func=user_view,
+    methods=["POST"],
+)
+app.add_url_rule(
     "/user",
-    view_func=UserView.as_view("user"),
-    methods=["GET", "POST", "PATCH", "DELETE"],
+    view_func=user_view,
+    methods=["GET", "PATCH", "DELETE"],
 )
 app.add_url_rule("/advertisements", view_func=advertisements_view, methods=["POST"])
 app.add_url_rule(
